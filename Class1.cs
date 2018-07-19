9a65cbeef9a9d9aeb7242f03379222ec3c68fb8d
Ben <ben.mann108@gmail.com>
2018-07-20 00:03:27 +0100

    Initial Commit - Working software

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.Activities;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+
+namespace PingActivity
+{
+    public class PingHost : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> HostName { get; set; }
+
+        [Category("Output")]
+        public OutArgument<bool> Success { get; set; }
+
+        [Category("Output")]
+        public OutArgument<System.Int32> DelayMS { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var hostName = HostName.Get(context);
+            bool success;
+            Ping ping = null;
+
+            try
+            {
+                ping = new Ping();
+                PingReply reply;
+
+                reply = ping.Send(hostName);
+                success = reply.Status == IPStatus.Success;
+
+                if (success)
+                {
+                    DelayMS.Set(context, (int)reply.RoundtripTime);
+                }
+
+                Success.Set(context, success);
+            }
+            catch (System.Exception)
+            {
+                Success.Set(context, false);
+            }
+            finally
+            {
+                if (ping != null)
+                {
+                    ping.Dispose();
+                }
+            }
+        }
+    }
+}
